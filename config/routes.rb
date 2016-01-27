3c1501980b9a036e67a54686ce563fc1f10354b0
Raa Loca <raalocaloca@gmail.com>
2016-01-27 11:16:49 +0530

    finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'signup' =>  'users#new'
 
   root 'static_pages#home'
@@ -8,6 +10,12 @@ Rails.application.routes.draw do
   get 'about'=> 'static_pages#about'
 
   get 'contact'=> 'static_pages#contact'
+
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 
