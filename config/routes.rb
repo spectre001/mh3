9eaaa2cb26b9570846bde52938b66eb32848b80c
Raa Loca <raalocaloca@gmail.com>
2016-01-27 05:54:27 +0530

    Finish user sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   get 'contact'=> 'static_pages#contact'
 
+  resources :users
 
 
   # The priority is based upon order of creation: first created -> highest priority.
